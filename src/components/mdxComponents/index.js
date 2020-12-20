6d9f1a389d8474273be83695f98a920ea4ef7763
Harvey <15339206608@163.com>
2020-12-20 12:56:42 +0800

    basic types

--- src/components/mdxComponents/index.js
+++ src/components/mdxComponents/index.js
@@ -9,25 +9,30 @@ const StyledPre = styled('pre')`
   background: ${props => props.theme.colors.preFormattedText};
 `;
 
+function getId(props) {
+  if (Array.isArray(props.children)) {
+    return props.children
+      .map(item => {
+        if (item.$$typeof) {
+          return item.props.children;
+        } else {
+          return item;
+        }
+      })
+      .join('')
+      .replace(/\s+/g, '')
+      .toLowerCase();
+  }
+  return props.children.replace(/\s+/g, '').toLowerCase();
+}
+
 export default {
-  h1: props => (
-    <h1 className="heading1" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
-  ),
-  h2: props => (
-    <h2 className="heading2" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
-  ),
-  h3: props => (
-    <h3 className="heading3" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
-  ),
-  h4: props => (
-    <h4 className="heading4" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
-  ),
-  h5: props => (
-    <h5 className="heading5" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
-  ),
-  h6: props => (
-    <h6 className="heading6" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
-  ),
+  h1: props => <h1 className="heading1" id={getId(props)} {...props} />,
+  h2: props => <h2 className="heading2" id={getId(props)} {...props} />,
+  h3: props => <h3 className="heading3" id={getId(props)} {...props} />,
+  h4: props => <h4 className="heading4" id={getId(props)} {...props} />,
+  h5: props => <h5 className="heading5" id={getId(props)} {...props} />,
+  h6: props => <h6 className="heading6" id={getId(props)} {...props} />,
   p: props => <p className="paragraph" {...props} />,
   pre: props => (
     <StyledPre>
